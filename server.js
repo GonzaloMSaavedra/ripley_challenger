1957897021600823be3b0e48a2a4af7e806c3ecf
Gonzalo Maldonado Saavedra <gonzalo_94115@hotmail.com>
2019-04-30 13:52:09 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
 const http = require("http");
 const app = require("./app");
 
-
-app.set('port', process.env.PORT || 3000)
-
-
+const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 
-server.listen(app.get('port'), () => {
-
-});
+server.listen(port);
