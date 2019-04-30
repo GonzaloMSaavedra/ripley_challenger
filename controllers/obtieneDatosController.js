fb87b9012363819abc1e6af998e71b1e665410e9
Gonzalo Maldonado Saavedra <gonzalo_94115@hotmail.com>
2019-04-30 10:49:04 -0300

    Update obtieneDatosController.js

--- controllers/obtieneDatosController.js
+++ controllers/obtieneDatosController.js
@@ -5,7 +5,7 @@ const router = express.Router();
 var getJSON = require('get-json');
 //PARTE PARA CONECTAR CON REDIS 
 let redis = require('redis');
-let client = redis.createClient();
+let client = redis.createClient(process.env.REDIS_URL);
 
 
 //ESTA FUNCION SE EJECUTA DESDE EL APP.JS AL INICAR LA APLICACION Y GUARDA LA LATITUD Y LONGITUD EN REDIS
