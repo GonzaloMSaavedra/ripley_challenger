561609f7586a65833a261d4afe5c6bbc111d7b5a
Gonzalo Maldonado Saavedra <gonzalo_94115@hotmail.com>
2019-04-30 18:22:16 -0300

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,16 @@ obtieneDatosController.guardaCordenadas;
 //
 
 
+//CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
 // parse application/json
