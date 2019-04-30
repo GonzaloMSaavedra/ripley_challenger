8e38336899842b0e5b4beb2ea94fea76b77288a4
Gonzalo Maldonado Saavedra <gonzalo_94115@hotmail.com>
2019-04-30 15:49:12 -0300

    Update app.js

--- app.js
+++ app.js
@@ -11,15 +11,6 @@ let obtieneDatosController = require('./controllers/obtieneDatosController');
 obtieneDatosController.guardaCordenadas;
 //
 
-//CONTROL DE ACCESO
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
-
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
